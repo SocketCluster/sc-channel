8476a8213068e7191f4fb34237ac4ab9cffcc625
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-11-11 01:10:04 -0500

    WIP

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-var Emitter = require('component-emitter');
+var IterableAsyncStream = require('iterable-async-stream');
 
 var SCChannel = function (name, client, options) {
-  var self = this;
-
-  Emitter.call(this);
+  IterableAsyncStream.call(this);
 
   this.PENDING = 'pending';
   this.SUBSCRIBED = 'subscribed';
@@ -13,11 +11,12 @@ var SCChannel = function (name, client, options) {
   this.state = this.UNSUBSCRIBED;
   this.client = client;
 
+  this.listeners = {};
   this.options = options || {};
   this.setOptions(this.options);
 };
 
-SCChannel.prototype = Object.create(Emitter.prototype);
+SCChannel.prototype = Object.create(IterableAsyncStream.prototype);
 
 SCChannel.prototype.setOptions = function (options) {
   if (!options) {
@@ -28,6 +27,26 @@ SCChannel.prototype.setOptions = function (options) {
 
   if (options.data !== undefined) {
     this.data = options.data;
+  }
+};
+
+SCChannel.prototype.listener = function (eventName) {
+  var currentListener = this.listeners[eventName];
+  if (!currentListener) {
+    currentListener = new IterableAsyncStream();
+    this.listeners[eventName] = currentListener;
+  }
+  return currentListener;
+};
+
+SCChannel.prototype.destroyListener = function (eventName) {
+  delete this.listeners[eventName];
+};
+
+SCChannel.prototype.emit = function (event, data) {
+  var listener = this.listeners[event];
+  if (listener) {
+    listener.write(data);
   }
 };
 
@@ -49,18 +68,6 @@ SCChannel.prototype.isSubscribed = function (includePending) {
 
 SCChannel.prototype.publish = function (data, callback) {
   this.client.publish(this.name, data, callback);
-};
-
-SCChannel.prototype.watch = function (handler) {
-  this.client.watch(this.name, handler);
-};
-
-SCChannel.prototype.unwatch = function (handler) {
-  this.client.unwatch(this.name, handler);
-};
-
-SCChannel.prototype.watchers = function () {
-  return this.client.watchers(this.name);
 };
 
 SCChannel.prototype.destroy = function () {
